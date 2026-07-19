ad7e6c39dc84d81483a1bbdc4dbd32fb6fd0adf0
Jeffry Royce <coachroyce@gmail.com>
2026-07-19 17:58:17 -0500

    Change default paging for /search to 48 per

--- app/routes/search.py
+++ app/routes/search.py
@@ -15,7 +15,7 @@ def search():
     Args:
         q (str): The search query string.
         page (int): The page number for pagination (default is 1).
-        limit (int): The number of results per page (default is 24).
+        limit (int): The number of results per page (default is 48).
 
     Returns:
         JSON response containing:
@@ -26,7 +26,7 @@ def search():
     """
     q = escape(request.args.get('q', '').strip())
     page = int(request.args.get('page', 1))
-    limit = int(request.args.get('limit', 24))
+    limit = int(request.args.get('limit', 48))
     offset = (page - 1) * limit
 
     # Base query for filtering
