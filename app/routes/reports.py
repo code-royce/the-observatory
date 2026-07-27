c0194382d6bc5cf87370e7f4987f526af7f5cdaf
Kristin Wiseman <88798340+kristin-wiseman@users.noreply.github.com>
2026-07-27 10:32:53 -0500

    Fix reports route and add pagination to it

--- app/routes/reports.py
+++ app/routes/reports.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, jsonify, request
-from markupsafe import escape
 
 from app.utils import get_db_connection, handle_db_errors
 
@@ -9,17 +8,42 @@ reports_bp = Blueprint('reports', __name__)
 @handle_db_errors
 def reports():
     """
-    Returns all tuples form the CommunityReports table
-    """
-    # Create a querry to get the reports to appear in the community reports
-    query_reports = f"""SELECT *
-                        FROM CommunityReport"""
+    Returns all tuples form the CommunityReport table
 
-    # Use the above to querry the gcp database
+    Returns:
+        JSON response containing:
+            - data: list of all community reports as dictionaries
+            - total: total number of results
+            - page: current page number
+            - limit: number of results per page
+    """
+    page = int(request.args.get('page', 1))
+    limit = int(request.args.get('limit', 48))
+    offset = (page - 1) * limit
+
+    # Query to get all community reports, with pagination
+    query_reports = f"""SELECT *
+                        FROM CommunityReport
+                        LIMIT %s
+                        OFFSET %s"""
+
+    # Get total count to help React manage pagination
+    count_query = f"""
+        SELECT COUNT(*) AS total
+        FROM CommunityReport"""
+
+    # Use the above to query the gcp database
     with get_db_connection() as conn:
         with conn.cursor(dictionary=True) as cursor:
-            cursor.execute(query_reports)
+            cursor.execute(query_reports, (limit, offset))
             reports_results = cursor.fetchall()
 
+            cursor.execute(count_query)
+            total = cursor.fetchone()['total']
 
-    return jsonify(reports_results)
+    return jsonify({
+        "data": reports_results,
+        "total": total,
+        "page": page,
+        "limit": limit
+    })
